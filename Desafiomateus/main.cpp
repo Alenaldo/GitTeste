04c3f7ddaba186dde732d1e08ec45826b06c50cf
unknown <alenaldomf@gmail.com>
2018-08-23 12:30:14 -0300

    Desafio Mateus

--- Desafiomateus/main.cpp
+++ Desafiomateus/main.cpp
@@ -1,5 +1,3 @@
-
-
 #include <cstdlib>
 #include <stdlib.h>
 #include <iostream>
@@ -12,41 +10,26 @@ void VerificarEstoqueFilial();
 void cadastroFilial();
 void cadastroProduto();
 
-typedef struct filial {
-    int idfilial;
-    char Nomefilial;
-    EstoqueF estoque;
-}Filial;
+#define QTD  5
 
-typedef struct produto {
+
+typedef struct {
     int idproduto;
-    char Nomeproduto;    
+    char Nomeproduto;
+    int quantDisponivel;
+    int quantReservada;
+    int quantTotal;
 }Produto;
 
-typedef struct pedidoS{
-    int idPedido;
-    char nomeProduto;
-    int idProduto;
-    int quant;
-}PedidoSaida;
 
-typedef struct pedidoE{
-    int idPedido;
-    int idProduto;
-    char nomeProduto;
-    int quant;
-}PedidoEntrada;
 
-typedef struct EstoqueF{
-   // int idProduto;
-    //char nomeProduto;
-    Produto produto;
-    int quantDisponivel;
-    int quantResevada;
-    int quantFinal;
-}EstoqueFilial;
+typedef struct filial{
+    int idfilial;
+    char Nomefilial;
+    Produto ProdutoEstoque[10];
+}Filial;
+Filial FilialPedido[10];
 
-//Filial quant;
 
 int main(int argc, char** argv) 
 {
@@ -64,101 +47,116 @@ int main(int argc, char** argv)
     cout<<"*-----------------------------*"<<endl;
     cout<<"ESCOLHA UMA OPCAO? "<<endl;
     cin>>opM;
+    
     system("clear");
-    switch (opM);
+    
+    switch (opM)
     {
 	case 1 :
             {
             system("clear");
-            criaPedidoSaida();
+            criaPedidoSaida();        
             break;
-            }
-        case 2 :
+        }   
+        case 2:
             {
             system("clear");
             criaPedidoEntrada();
             break;
             }
-        case 3 :
+        case 3:
            {
             system("clear");
             VerificarEstoqueFilial();
             break;
             }
         
-        case 4 :
+        case 4:
            {
             system("clear");
             cadastroFilial();
             break;
             }
-        case 5 :
+        case 5:
            {
             system("clear");
             cadastroProduto();
             break;
             }
         default :
+        {
             cout<<"Opcao Invalida"<<endl;
+        }
     }   
-    //return 0;
+    return 0;
 }
             
 void criaPedidoSaida(){
     
-    int idfilial,idproduto,quant;
+    int idf,idp,quant;
     
     cout<<"|======================| CRIA PEDIDO ESTOQUE SAIDA |======================|"<<endl;
     cout<<"*=============================================================================*"<<endl;
-    cout<<"Informe  o id da filial"<<endl;   
-    cin>>idfilial;    
+    cout<<"Informe  o id da filial"<<endl;
+    cin>>idf;
     cout<<"Informe  o produto"<<endl;  
-    cin>>idproduto;
+    cin>>idp;
     cout<<"Informe  a quantidade do produto"<<endl;
-    cin>>quant;
+    cin>>quant;    
     
-    
-    
-    
+     FilialPedido[idf].ProdutoEstoque[idp].quantTotal =  FilialPedido[idf].ProdutoEstoque[idp].quantTotal - quant;  
     
 }
 
 void criaPedidoEntrada(){
     
-    int idfilial,idproduto,quant;
+    int idf,idp,quant;
     
     cout<<"|======================| CRIA PEDIDO ESTOQUE ENTRADA |======================|"<<endl;
     cout<<"*=============================================================================*"<<endl;
     cout<<"Informe  o id da filial"<<endl;   
-    cin>>idfilial;    
+    cin>>idf;    
     cout<<"Informe  o produto"<<endl;  
-    cin>>idproduto;
+    cin>>idp;
     cout<<"Informe  a quantidade do produto"<<endl;
-    cin>>quant;
+    cin>>quant; 
     
-    
+    FilialPedido[idf].ProdutoEstoque[idp].quantTotal = FilialPedido[idf].ProdutoEstoque[idp].quantTotal + quant;    
     
 }
 
 void VerificarEstoqueFilial(){
     
-    
+     int idf,idp;
+    cout<<"|======================| CRIA PEDIDO ESTOQUE ENTRADA |======================|"<<endl;
+    cout<<"*=============================================================================*"<<endl;
+    cout<<"Informe  o id da filial"<<endl;   
+    cin>>idf;    
+    cout<<"Informe  o produto"<<endl;  
+    cin>>idp;
+    cout <<"Tem "<<FilialPedido[idf].ProdutoEstoque[idp].quantTotal<< " deste Produto nessa filial"<<endl;
+    cout <<"Tem "<<FilialPedido[idf].ProdutoEstoque[idp].quantReservada<< " deste Produto nessa filial no Resevado"<<endl;
+    cout <<"Tem "<<FilialPedido[idf].ProdutoEstoque[idp].quantDisponivel<< " deste Produto nessa filial no Disponivel"<<endl;   
     
 }
 
 void cadastroFilial(){
-    
+    int i=0;
+    char nome;
     cout<<"Informe o nome para sua filial"<<endl;
-    cin>>filial.Nomefilial;
-    filial.idfilial++;   
+    cin>>nome;
+    FilialPedido[i].Nomefilial = nome;
     cout<<"Filial Cadastrada!"<<endl;
+    i++;
 }
 
 void cadastroProduto(){
-    
-    cout<<"Informe o nome do produto"<<endl;
-    cin>>produto.Nomeproduto;
-    produto.idproduto++;   
-    cout<<"Produto Cadastrada!"<<endl;
+    int j=0;
+    char nome;
+    cout<<"Informe o nome para sua Produto"<<endl;
+    cin>>nome;
+   // produto[j].Nomeproduto = nome; 
+    cout<<"Produto` Cadastrada!"<<endl;
+    j++;
 }
 
--- Desafiomateus/nbproject/Makefile-Debug.mk
+++ Desafiomateus/nbproject/Makefile-Debug.mk
@@ -35,7 +35,6 @@ OBJECTDIR=${CND_BUILDDIR}/${CND_CONF}/${CND_PLATFORM}
 
 # Object Files
 OBJECTFILES= \
-	${OBJECTDIR}/base.o \
 	${OBJECTDIR}/main.o
 
 
@@ -62,11 +61,6 @@ LDLIBSOPTIONS=
 ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/desafiomateus.exe: ${OBJECTFILES}
 	${MKDIR} -p ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}
 	${LINK.cc} -o ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/desafiomateus ${OBJECTFILES} ${LDLIBSOPTIONS}
-
-${OBJECTDIR}/base.o: base.cpp
-	${MKDIR} -p ${OBJECTDIR}
-	${RM} "$@.d"
-	$(COMPILE.cc) -g -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/base.o base.cpp
 
 ${OBJECTDIR}/main.o: main.cpp
 	${MKDIR} -p ${OBJECTDIR}
